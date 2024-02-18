9b60803c6b54b18704d47a1cb28031fb056c2e7f
Akash Kumar <123872296+akashkumar7993@users.noreply.github.com>
2024-02-18 20:25:58 +0530

    Added Module 2 Igniting app

---
+++ Module 2. Igniting our App/1. app/app.js
@@ -0,0 +1,19 @@
+/*
+*<div id="parent">
+*       <div id="child">
+*           <h1>Nested html structure using react</h1>
+*           <h2>Nested html structure using react</h2>
+*       </div>
+*</div>
+*
+* ReactElement(object) => HTML(Browser Understands)
+*/
+
+const parent = React.createElement("div", {id: "parent"}, 
+React.createElement("div", {id: "child"}, 
+[React.createElement("h1", {}, "Nested html structure using react"),
+React.createElement("h2", {}, "Nested html structure using react")
+])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
