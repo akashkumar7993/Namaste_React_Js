5ba5e032a141dca782fdbfd00f606bed58303b5e
Akash Kumar <123872296+akashkumar7993@users.noreply.github.com>
2024-02-18 19:04:01 +0530

    Added Module 1 Inception

--- Module 1. Inception/4. Nested html Structure using React/app.js
+++ Module 1. Inception/4. Nested html Structure using React/app.js
@@ -9,12 +9,39 @@
 * ReactElement(object) => HTML(Browser Understands)
 */
 
+// const parent = React.createElement("div", {id: "parent"}, 
+// React.createElement("div", {id: "child"}, 
+// [React.createElement("h1", {}, "Nested html structure using react"),
+// React.createElement("h2", {}, "Nested html structure using react")
+// ])
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+// /*
+//  * <div id="parent">
+// *       <div id="child">
+// *           <h1>Nested html structure using react</h1>
+// *           <h2>Nested html structure using react</h2>
+// *       </div>
+//        <div id="child">
+// *           <h1>Nested html structure using react</h1>
+// *           <h2>Nested html structure using react</h2>
+// *       </div>
+// *</div>
+//  * 
+//  * /
+ 
+
 const parent = React.createElement("div", {id: "parent"}, 
-React.createElement("div", {id: "child"}, 
+[React.createElement("div", {id: "child 1"}, 
+[React.createElement("h1", {}, "Nested html structure using react"),
+React.createElement("h2", {}, "Nested html structure using react")
+]),
+React.createElement("div", {id: "child 2"}, 
 [React.createElement("h1", {}, "Nested html structure using react"),
 React.createElement("h2", {}, "Nested html structure using react")
 ])
-);
-
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
