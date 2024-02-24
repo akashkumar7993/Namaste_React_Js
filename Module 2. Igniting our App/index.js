d0973755df7c95f30206bbf53c868d7b909f3355
akashkumar7993 <akumar7993@gmail.com>
2024-02-24 17:42:16 +0530

    breif description about parcel

---
+++ Module 2. Igniting our App/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement("h1", {id: "title"}, "Heading");
+
+const root = ReactDOM.createElement(document.getElementById("root"));
+root.render(container);
