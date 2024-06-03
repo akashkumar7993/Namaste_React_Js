7c931cebcc5940be412dd30695421aafa026996b
akashkumar7993 <akumar7993@gmail.com>
2024-06-03 22:14:12 +0530

    error handlimg

--- Module 5. Let's get Hooked/Components/Cart.js
+++ Module 5. Let's get Hooked/Components/Cart.js
@@ -1,7 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
-import ItemList from "./itemList";
+import ItemList from "./ItemList";
 import { clearCart } from "../utils/cartSlice";
-import { UseDispatch } from "react-redux";
 
 const Cart = () => {
 
